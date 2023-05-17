0cb58843022704587987a6d92db39ef2ebb7cecb
ys05059 <ys05059@gmail.com>
2023-05-18 02:20:02 +0900

    feat : CustomCallAdapter for response handling

--- app/src/main/java/com/knu/quickthink/di/Module.kt
+++ app/src/main/java/com/knu/quickthink/di/Module.kt
@@ -5,16 +5,61 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.knu.quickthink.R
+import com.knu.quickthink.network.networkResultCallAdapter.NetworkResultCallAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
+import java.io.IOException
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object Module{
+    private const val BASE_URL = "https://api.quickthink.online"
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(NetworkResultCallAdapterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        authInterceptor: AuthInterceptor
+    ): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            this.setLevel(HttpLoggingInterceptor.Level.BODY)
+        }
+        return OkHttpClient.Builder()
+//            .connectTimeout(100, TimeUnit.MILLISECONDS)
+//            .readTimeout(100, TimeUnit.MILLISECONDS)
+//            .writeTimeout(100, TimeUnit.MILLISECONDS)
+            .addInterceptor(logging)
+            .addNetworkInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideAuthInterceptor(
+    ): AuthInterceptor {
+        return AuthInterceptor()
+    }
 
     @Provides
     @Singleton
@@ -24,5 +69,23 @@ object Module{
             .requestEmail()
             .build()
         return GoogleSignIn.getClient(context, gso)
+    }
+}
+
+class AuthInterceptor : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val newRequest = chain.request().newBuilder()
+//            .addHeader("Authorization", "Bearer $sessionId")
+            .build()
+
+        val response = chain.proceed(newRequest)
+        if(response.header("Authorization") != null){
+            // token이 vaild할 경우 sessionId를 저장해줘야함
+//            sessionManager.setSessionId(response.header("Authorization")!!)
+            Timber.tag("login").d("header Authorization : ${response.header("Authorization")!!}")
+        }
+        // token이 invalid 하면  이미 response 객체에 오류가 붙어서 올거임-> NetworkCallAdapter가 그 뒤에 처리할거임
+        return response
     }
 }
