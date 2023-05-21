726883e5722681c79adffad87c426f2a3852ca79
ys05059 <ys05059@gmail.com>
2023-05-21 15:09:25 +0900

    feat : scroll to current cursor in textField

--- app/src/main/java/com/knu/quickthink/screens/card/CardEditScreen.kt
+++ app/src/main/java/com/knu/quickthink/screens/card/CardEditScreen.kt
@@ -1,31 +1,40 @@
 package com.knu.quickthink.screens.card
 
 import android.app.Activity
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusOrder
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.*
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.halilibo.richtext.markdown.Markdown
 import com.halilibo.richtext.ui.RichText
 import com.knu.quickthink.R
 import com.knu.quickthink.screens.main.addFocusCleaner
+import kotlinx.coroutines.CoroutineScope
 import timber.log.Timber
+import java.time.format.TextStyle
 
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -36,31 +45,54 @@ fun CardEditScreen(
     val keyboardController = LocalSoftwareKeyboardController.current
     val content by viewModel.content.collectAsState()
     val isEditing by viewModel.isEditing.collectAsState()
-
     val context = LocalContext.current
 
-    DisposableEffect(Unit){
+    // 키보드에 따라 window크기 조절을 위해 필요한 부분으로 이해함
+    DisposableEffect(Unit) {
         val window = (context as? Activity)?.window
-        if(window != null){
-            WindowCompat.setDecorFitsSystemWindows(window,false)
+        if (window != null) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
         }
-        onDispose {  }
+        onDispose { }
     }
     val isKeyboardOpen by keyboardAsState()
-    val focusRequester = remember{ FocusRequester() }
+    val focusRequester = remember { FocusRequester() }
+    val scrollState = rememberScrollState()
+    val lineHeightPx = with(LocalDensity.current) { 24.dp.toPx() }
+    val imeHeight = WindowInsets.ime.getBottom(LocalDensity.current)
+    Timber.tag("cardEdit").d("imeBottom $imeHeight")
 
-    /* isEditing true일 경우 텍스트필드에 focus 넣어주기 */
-    LaunchedEffect(isEditing){
+
+    LaunchedEffect(isEditing) {
         Timber.tag("cardEdit").d("LaunchedEffect isEditing $isEditing")
-        if(isEditing){
-            focusRequester.requestFocus()
+        if (isEditing) {
+            /* isEditing true일 경우 텍스트필드에 focus 넣어주기 */
+//            focusRequester.requestFocus()
+//            scrollState.animateScrollTo(cursorPositionToPixelOffset(content,content.selection.start,lineHeightPx))    // 적용 잘 안됨
         }
     }
 
-    LaunchedEffect(isKeyboardOpen){
+    /*
+    * TODO : View로 돌아가기 버튼 있어야할 듯 탭으로 주거나(github PR 쓰는거처럼)
+    * 현재는 커서 위치 정하고 수정한 후에 키보드 내리면 자동저장되는 시스템
+    * 커서 위치 무조건 정해야 나갈 수 있는데 커서 위치 정하기 전에 돌아갈 수 있도록 만드는게 좋다
+    * */
+
+    LaunchedEffect(isKeyboardOpen) {
         if (!isKeyboardOpen) {
             viewModel.updateContent()
         }
+    }
+
+    LaunchedEffect(content) {
+        Timber.tag("cardEdit").d("LauncedEffect content.selection : ${content.selection} ")
+        scrollState.animateScrollTo(
+            cursorPositionToPixelOffset(
+                content,
+                content.selection.start,
+                lineHeightPx
+            )
+        )
     }
 
     Column(
@@ -68,6 +100,8 @@ fun CardEditScreen(
             .fillMaxSize()
             .background(Color.White)
             .padding(dimensionResource(id = R.dimen.vertical_margin))
+            .imePadding()
+            .verticalScroll(scrollState)
             .addFocusCleaner(keyboardController!!)
     ) {
         TextField(
@@ -76,22 +110,33 @@ fun CardEditScreen(
             textStyle = MaterialTheme.typography.h5,
             onValueChange = {}
         )
-        if (isEditing){
-            TextField(
-                value = content,
-                onValueChange = { viewModel.editContent(it)},
-                placeholder = { Text("카드 내용을 입력해주세요") },
-                modifier = Modifier
-                    .focusRequester(focusRequester)
-            )
-        }else{
-            RichText(
-                modifier = Modifier.clickable { viewModel.startEdit() }
-            ) {
-                Markdown(content = content)
+        Crossfade(targetState = isEditing) { targetState ->
+            when (targetState) {
+                true -> {
+                    OutlinedTextField(
+                        value = content,
+                        onValueChange = { textFieldValue ->
+                            viewModel.editContent(textFieldValue)
+                        },
+                        placeholder = { Text("카드 내용을 입력해주세요") },
+                        modifier = Modifier
+                            .focusRequester(focusRequester)
+                    )
+                }
+                false -> {
+                    RichText(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clickable {
+                                /*TODO : 클릭 꾹 누르면 검은색으로 변하는데 그거 없애줘야함*/
+                                viewModel.startEdit()
+                            }
+                    ) {
+                        Markdown(content = content.text)
+                    }
+                }
             }
         }
-
     }
 }
 
@@ -99,6 +144,34 @@ fun CardEditScreen(
 fun keyboardAsState(): State<Boolean> {
     val isImeVisible = WindowInsets.ime.getBottom(LocalDensity.current) > 0
     return rememberUpdatedState(isImeVisible)
+}
+
+private fun cursorPositionToPixelOffset(
+    textFieldValue: TextFieldValue,
+    cursorPosition: Int,
+    lineHeightPx: Float
+): Int {
+    val lines = textFieldValue.text.split("\n")
+
+    var offset = 0
+
+    for (element in lines) {
+        Timber.tag("cardEdit").d("element : $element")
+        offset += element.length
+
+        if (offset >= cursorPosition) {
+            // Subtract the length of the current line from the offset
+            // to get the offset within the line itself
+            offset -= element.length
+            break
+        }
+
+        // Add 1 to account for the line break character
+        offset++
+    }
+
+    // Calculate the pixel offset based on line height
+    return (offset / lines.size.toFloat() * lineHeightPx).toInt()
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/knu/quickthink/screens/card/CardViewModel.kt
+++ app/src/main/java/com/knu/quickthink/screens/card/CardViewModel.kt
@@ -1,5 +1,6 @@
 package com.knu.quickthink.screens.card
 
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.knu.quickthink.repository.UserRepository
@@ -13,8 +14,8 @@ import javax.inject.Inject
 @HiltViewModel
 class CardViewModel @Inject constructor(
 ): ViewModel(){
-    private val _content = MutableStateFlow("")
-    val content : StateFlow<String> = _content.asStateFlow()
+    private val _content = MutableStateFlow(TextFieldValue(text = ""))
+    val content : StateFlow<TextFieldValue> = _content.asStateFlow()
 
     private val _isEditing = MutableStateFlow(false)
     val isEditing : StateFlow<Boolean> = _isEditing.asStateFlow()
@@ -23,7 +24,7 @@ class CardViewModel @Inject constructor(
         fetchContent()
     }
 
-    fun editContent(newContent : String){
+    fun editContent(newContent : TextFieldValue){
         _content.value = newContent
     }
 
@@ -37,7 +38,7 @@ class CardViewModel @Inject constructor(
     }
 
     fun fetchContent(){
-        _content.value = testMarkdown
+        _content.value = _content.value.copy(text = testMarkdown)
     }
     /* TODO : fetchContent  -> 처음 카드 내용 가져오기*/
 
@@ -45,6 +46,33 @@ class CardViewModel @Inject constructor(
 }
 val testMarkdown = """
     # Demo
+
+    Emphasis, aka italics, with *asterisks* or _underscores_. Strong emphasis, aka bold, with **asterisks** or __underscores__. Combined emphasis with **asterisks and _underscores_**. [Links with two blocks, text in square-brackets, destination is in parentheses.](https://www.example.com). Inline `code` has `back-ticks around` it.
+
+    1. First ordered list item
+    2. Another item
+        * Unordered sub-list.
+    3. And another item.
+        You can have properly indented paragraphs within list items. Notice the blank line above, and the leading spaces (at least one, but we'll use three here to also align the raw Markdown).
+
+    * Unordered list can use asterisks
+    - Or minuses
+    + Or pluses
+    ---
+
+    ```javascript
+    var s = "code blocks use monospace font";
+    alert(s);
+    ```
+
+    Markdown | Table | Extension
+    --- | --- | ---
+    *renders* | `beautiful images` | ![random image](https://picsum.photos/seed/picsum/400/400 "Text 1")
+    1 | 2 | 3
+
+    > Blockquotes are very handy in email to emulate reply text.
+    > This line is part of the same quote.
+     # Demo
 
     Emphasis, aka italics, with *asterisks* or _underscores_. Strong emphasis, aka bold, with **asterisks** or __underscores__. Combined emphasis with **asterisks and _underscores_**. [Links with two blocks, text in square-brackets, destination is in parentheses.](https://www.example.com). Inline `code` has `back-ticks around` it.
 
